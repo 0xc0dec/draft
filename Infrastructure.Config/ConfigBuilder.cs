798ef6d9c44403d4ac632c246a9af7d9a0236a34
Aleksey Fedotov <alexey.fedotov@fastdev.se>
2018-03-25 20:23:02 +0500

    Renamed config files

--- Infrastructure.Config/ConfigBuilder.cs
+++ Infrastructure.Config/ConfigBuilder.cs
@@ -5,12 +5,12 @@ namespace Infrastructure.Config
 {
     public static class ConfigBuilder
     {
-        public static IConfigurationRoot Build(string[] cmdArgs, string workingDir)
+        public static IConfigurationRoot Build(string[] cmd, string workingDir)
         {
             return new ConfigurationBuilder()
-                .AddJsonFile(Path.Combine(workingDir, "config.json"), true, true)
-                .AddJsonFile(Path.Combine(workingDir, "config.local.json"), true, true)
-                .AddCommandLine(cmdArgs)
+                .AddJsonFile(Path.Combine(workingDir, "settings.json"), true, true)
+                .AddJsonFile(Path.Combine(workingDir, "settings.local.json"), true, true)
+                .AddCommandLine(cmd)
                 .AddEnvironmentVariables()
                 .Build();
         }
